405e8394880c9f177fe2f030706ec5a3ba4a0184
Arkhsat <arkhananta27@gmail.com>
2024-08-11 23:36:59 +0700

    OK

--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,3 @@
-import cards from "../assets/images/cards.png";
-import empty from "../assets/images/empty.png";
 import wom1 from "../assets/images/1.jpg";
 import wom3 from "../assets/images/3.jpg";
 import wom4 from "../assets/images/4.jpg";
@@ -13,4 +11,4 @@ import man8 from "../assets/images/8.jpg";
 import pat from "../assets/images/pattern.png";
 
 
-export default { cards, empty, wom1, wom3, wom4, wom5, wom6, wom9, wom10, man2, man7, man8, pat };
+export default { wom1, wom3, wom4, wom5, wom6, wom9, wom10, man2, man7, man8, pat };
